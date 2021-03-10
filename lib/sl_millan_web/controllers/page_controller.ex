641d7d9e2e9e98574c927bdbef4bc1de1ac746b2
César Millán <cesar.millan06@gmail.com>
2021-03-10 02:01:58 -0600

    Removed autogenerated files and tests

--- lib/sl_millan_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule SlMillanWeb.PageController do
-  use SlMillanWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- lib/sl_millan_web/views/page_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule SlMillanWeb.PageView do
-  use SlMillanWeb, :view
-end
